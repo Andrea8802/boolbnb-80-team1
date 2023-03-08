10e3d7799e65cb5e04e22ff132ae5b3757e7fe1d
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-03-08 12:24:15 +0100

    Rimosso email_verified_at

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,9 +43,7 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    // protected $casts = [
-    //     'email_verified_at' => 'datetime',
-    // ];
+
     public function apartments()
     {
         return $this->hasMany(Apartment::class);
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,9 +22,7 @@ class UserFactory extends Factory
             'surname' => fake()->lastName(),
             'date_of_birth' => fake()->date(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            // password
             'remember_token' => Str::random(10),
             'description' => fake()->text(200),
         ];
