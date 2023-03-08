347254abde8d55d5df14045464ba479b8b936b19
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-03-08 12:20:43 +0100

    Rimossa colonna email_verified_at;

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration {
             $table->string('surname')->nullable();
             $table->date("date_of_birth");
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('description')->nullable();
             $table->string('avatar')->default('avatar5.png');
