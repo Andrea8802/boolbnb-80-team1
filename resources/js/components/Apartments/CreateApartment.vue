db640f5d6be3793c7ce7f682e94c3a215d65c3bb
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-03-09 18:49:08 +0100

    Creata le classi per la grafica del form e prime modifiche grafiche apportate al form;

--- resources/js/components/Apartments/CreateApartment.vue
+++ resources/js/components/Apartments/CreateApartment.vue
@@ -1,84 +1,97 @@
+<style lang="scss" scoped>
+@use '/resources/sass/variables.scss' as *;
+
+.ms_title_page {
+    font-weight: 900;
+    color: $principalColor;
+}
+
+.ms_label_bg {
+    background-color: $principalColor;
+    color: #ffffff;
+    border-color: $principalColor;
+    font-weight: 600;
+}
+
+.ms_ctn_input {
+    width: 400px;
+    margin: 0 auto;
+}
+
+.ms_input_focus_color:focus {
+    color: $principalColor;
+    border-color: $principalColor;
+    box-shadow: 0 0 10px $principalColor;
+}
+</style>
+
 <template>
-    <!-- <h1 @click="log">CIAO</h1>
-    <ul>
-        <li v-for="service in services">
-            {{ service.name }}
-        </li>
-    </ul> -->
-    <h1>Create a New Apartment</h1> <br>
-    <form action=""
-        enctype="multipart/form-data"
-        @submit.prevent="createApartment"
-        method="post">
-        <label for="title">Title : </label>
-        <input type="text"
-            name="title"
-            v-model="modelTitle"> <br> <br>
-        <label for="description">Description : </label>
-        <input type="text"
-            name="description"
-            v-model="modelDescription"> <br> <br>
-        <label for="price">Price : </label>
-        <input type="number"
-            name="price"
-            v-model="modelPrice"> <br> <br>
-        <label for="beds_num">Beds Number : </label>
-        <input type="number"
-            name="beds_num"
-            v-model="modelBedsNum"> <br> <br>
-        <label for="rooms_num">Rooms Number : </label>
-        <input type="number"
-            name="rooms_num"
-            v-model="modelRoomsNum"> <br> <br>
-        <label for="baths_num">Baths Number : </label>
-        <input type="number"
-            name="baths_num"
-            v-model="modelBathsNum"> <br> <br>
-        <label for="size">Size : </label>
-        <input type="number"
-            name="size"
-            v-model="modelSize"> <br> <br>
-        <label for="address">Address : </label>
-        <input type="text"
-            name="address"
-            v-model="modelAddress"> <br> <br>
-        <label for="lat">Lat : </label>
-        <input type="number"
-            name="lat"
-            v-model="modelLat"> <br> <br>
-        <label for="long">Long : </label>
-        <input type="number"
-            name="long"
-            v-model="modelLong"> <br> <br>
-        <label for="long">Apartment Image : </label>
-        <input type="file"
-            name="long"
-            v-on:change="onImageChange"> <br> <br>
-        <label for="sponsors">Select a Sponsor : </label>
-        <select name="imageApartment"
-            v-model="modelSponsor">
-            <option v-for="sponsor in sponsors"
-                :value="sponsor.id"
-                :key="sponsor.id">
-                {{ sponsor.name }}
-            </option>
+    <div class="container text-center">
+        <h1 class="ms_title_page mb-5">Crea il tuo Spazio</h1>
+
+        <!-- container del form per creare un appartamento -->
+        <div>
+            <form action="" enctype="multipart/form-data" @submit.prevent="createApartment" method="post">
+
+                <div class="ms_ctn_input input-group mb-3">
+                    <label class="ms_label_bg input-group-text" id="basic-addon1" for="title">Titolo : </label>
+                    <input type="text" name="title" v-model="modelTitle" class="form-control ms_input_focus_color"
+                        placeholder="Inserisci un titolo..." aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+
+                <div class="ms_ctn_input input-group mb-3">
+                    <label class="ms_label_bg input-group-text" id="basic-addon1" for="description">Descrizione : </label>
+                    <input type="text" name="description" v-model="modelDescription"
+                        class="form-control ms_input_focus_color" placeholder="Inserisci una descrizione..."
+                        aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+
+                <div class="ms_ctn_input input-group mb-3">
+                    <label class="ms_label_bg input-group-text" id="basic-addon1" for="price">Prezzo : </label>
+                    <input type="number" name="price" v-model="modelPrice" class="form-control ms_input_focus_color"
+                        placeholder="Inserisci un prezzo..." aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+
+                <div>
+
+                </div>
+
+                <label for="beds_num">Numero di letti : </label>
+                <input type="number" name="beds_num" v-model="modelBedsNum"> <br> <br>
+                <label for="rooms_num">Numero di Stanze : </label>
+                <input type="number" name="rooms_num" v-model="modelRoomsNum"> <br> <br>
+                <label for="baths_num">Numero di bagni : </label>
+                <input type="number" name="baths_num" v-model="modelBathsNum"> <br> <br>
+                <label for="size">Grandezza(mq) : </label>
+                <input type="number" name="size" v-model="modelSize"> <br> <br>
+                <label for="address">Indirizzo : </label>
+                <input type="text" name="address" v-model="modelAddress"> <br> <br>
+                <label for="lat">Latitudine : </label>
+                <input type="number" name="lat" v-model="modelLat"> <br> <br>
+                <label for="long">Longitudine : </label>
+                <input type="number" name="long" v-model="modelLong"> <br> <br>
+                <label for="long">Immagine di Copertina : </label>
+                <input type="file" name="long" v-on:change="onImageChange"> <br> <br>
+                <label for="sponsors">Seleziona il tipo di Sponsor : </label>
+                <select name="imageApartment" v-model="modelSponsor">
+                    <option v-for="sponsor in sponsors" :value="sponsor.id" :key="sponsor.id">
+                        {{ sponsor.name }}
+                    </option>
 
 
-        </select> <br> <br>
+                </select> <br> <br>
 
-        <label for="">Tags : </label> <br>
+                <label for="">Servizi : </label> <br>
 
-        <div v-for="service in services">
-            <input type="checkbox"
-                :value="service.id"
-                name=services
-                v-model="modelServices">
-            <label for="services">{{ service.name }}</label>
-        </div> <br>
+                <div v-for="service in services">
+                    <input type="checkbox" :value="service.id" name=services v-model="modelServices">
+                    <label for="services">{{ service.name }}</label>
+                </div> <br>
 
-        <input type="submit"
-            value="create">
-    </form>
+                <input type="submit" value="Crea">
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
