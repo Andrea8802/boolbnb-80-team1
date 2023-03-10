7e80dd6b5b0bab55be36d351b8b97db2f30f5d27
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-03-10 17:36:43 +0100

    Impostata la struttura del footer;

--- resources/js/components/welcome_components/welcomeFooter.vue
+++ resources/js/components/welcome_components/welcomeFooter.vue
@@ -1,1 +1,23 @@
-<template></template>
+<script>
+
+</script>
+
+<template>
+    <div class="container-fluid ms_ctn_footer">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '/resources/sass/variables.scss' as *;
+
+.ms_ctn_footer {
+    border-top: solid 2px $principalColor;
+    height: 60px;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: 999;
+}
+</style>
