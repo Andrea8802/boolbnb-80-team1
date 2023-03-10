d6af4ae7562dca722507c2e2a0183168957e7b8b
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-03-10 17:45:27 +0100

    Fix alla grafica di welcome page;

--- resources/js/components/welcome_components/welcomeFooter.vue
+++ resources/js/components/welcome_components/welcomeFooter.vue
@@ -3,21 +3,21 @@
 </script>
 
 <template>
-    <div class="container-fluid ms_ctn_footer">
+    <section>
+        <div class="container-fluid ms_ctn_footer">
 
-    </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
 @use '/resources/sass/variables.scss' as *;
 
-.ms_ctn_footer {
+section {
     border-top: solid 2px $principalColor;
+}
+
+.ms_ctn_footer {
     height: 60px;
-    position: fixed;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    z-index: 999;
 }
 </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,42 +12,44 @@
     </head>
     <body>
         {{-- conatiner principale della pagina --}}
-        <div class="container-fluid" id="app">
+        <div id="app">
             {{-- header --}}
             <header>
-                <div class="container-fluid ms_nav_welcome">
-                    {{-- logo pagina --}}
-                    <div class="ms_ctn_logo d-flex justify-content-start align-items-center">
-                        <div class="ms_slot_logo me-1">
-                            <img src="/storage/logo-boolbnb.png" alt="logo_boolbnb">
-                        </div>
-                        <div class="ms_slot_boolbnb_title d-none d-md-block">
-                            <h1>bollbnb</h1>
-                        </div>
-                    </div>
-
-                    {{-- serach bar --}}
-                    <div>
-                        
-                    </div>
-
-                    {{-- link login e dashboard --}}
-                    <div>
-                        @if (Route::has('login'))
-                            <div>
-                                @auth
-                                    <a href="{{ url('/home') }}" class="ms_link_nav_welcome">Dashboard</a>
-                                @else
-                                    <a href="{{ route('login') }}" class="ms_link_nav_welcome">Accedi</a>
-            
-                                    @if (Route::has('register'))
-                                        <a href="{{ route('register') }}" class="ms_link_nav_welcome">Registrati</a>
-                                    @endif
-                                @endauth
+                <section class="ms_section_welcome_header">
+                    <div class="container-fluid ms_nav_welcome">
+                        {{-- logo pagina --}}
+                        <div class="ms_ctn_logo d-flex justify-content-start align-items-center">
+                            <div class="ms_slot_logo me-1">
+                                <img src="/storage/logo-boolbnb.png" alt="logo_boolbnb">
                             </div>
-                        @endif
+                            <div class="ms_slot_boolbnb_title d-none d-md-block">
+                                <h1>bollbnb</h1>
+                            </div>
+                        </div>
+    
+                        {{-- serach bar --}}
+                        <div>
+                            
+                        </div>
+    
+                        {{-- link login e dashboard --}}
+                        <div>
+                            @if (Route::has('login'))
+                                <div>
+                                    @auth
+                                        <a href="{{ url('/home') }}" class="ms_link_nav_welcome">Dashboard</a>
+                                    @else
+                                        <a href="{{ route('login') }}" class="ms_link_nav_welcome">Accedi</a>
+                
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}" class="ms_link_nav_welcome">Registrati</a>
+                                        @endif
+                                    @endauth
+                                </div>
+                            @endif
+                        </div>
                     </div>
-                </div>
+                </section>
             </header>
 
             {{-- main --}}
