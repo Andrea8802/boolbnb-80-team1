3c040aa94b884803e26673c50fd1e1cc653168a6
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-03-13 18:05:55 +0100

    creato il componente messageApartment con le sue relative rotte;

---
+++ resources/js/components/messageApartment.vue
@@ -0,0 +1,19 @@
+<style lang="sccs" scoped>
+</style>
+
+<template>
+    <h1>Inserisci un messaggio</h1>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+    }
+}
+</script>
