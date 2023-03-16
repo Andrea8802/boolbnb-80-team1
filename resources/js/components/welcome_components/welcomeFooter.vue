7406fb7fd3a93099885a11d2798cfbd6bbb1b519
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-03-16 11:58:11 +0100

    Completato il responsive del footer;

--- resources/js/components/welcome_components/welcomeFooter.vue
+++ resources/js/components/welcome_components/welcomeFooter.vue
@@ -4,8 +4,8 @@
 
 <template>
     <section>
-        <div class="container-fluid ms_ctn_footer">
-            <div class="container h-100 d-flex justify-content-between align-items-center">
+        <div class="ms_ctn_footer">
+            <div class="container d-flex justify-content-between align-items-center ms_ctn_footer_responsive768">
                 <!-- slot dei link privacy, termini, mappa sito, dettagli azienda e destinazioni -->
                 <div class="ms_slot_link_footer_left">
                     <span>&#169; Boolbnb, Inc.</span>
@@ -22,7 +22,7 @@
                 </div>
 
                 <!-- slot dei link lingua, tipo moneta, supporto e risorse -->
-                <div class="ms_slot_link_footer_right d-flex align-items-center">
+                <div class="ms_slot_link_footer_right">
                     <a href="#">
                         <font-awesome-icon icon="fa-solid fa-globe" />
                         English(US)
@@ -30,10 +30,10 @@
                     <span class="mx-2">&#8226;</span>
                     <a href="#">&euro; EUR</a>
                     <span class="mx-2">&#8226;</span>
-                    <a href="#" class="d-flex align-items-center">
-                        <div class="me-2">
+                    <a href="#">
+                        <span>
                             Support & resources
-                        </div>
+                        </span>
                         <font-awesome-icon icon="fa-solid fa-angle-up" />
                     </a>
                 </div>
@@ -48,22 +48,44 @@
 .ms_ctn_footer {
     color: $secondColor;
     font-size: 14px;
-    height: 40px;
 
     a {
         color: $secondColor;
     }
 
-    .ms_slot_link_footer_left,
-    .ms_slot_link_footer_right {
-        width: fit-content;
+    .ms_slot_link_footer_left {
+        width: 55%;
     }
 
     .ms_slot_link_footer_right {
+        width: 40%;
 
         a {
             font-weight: 500;
         }
     }
 }
+
+// responsive footer sotto i 768px
+@media screen and (max-width: 768px) {
+    .ms_ctn_footer {
+        .ms_ctn_footer_responsive768 {
+            padding: 10px 10px;
+            align-items: start;
+            flex-direction: column !important;
+            gap: 10px;
+
+            .ms_slot_link_footer_left {
+                border-bottom: solid 1px $secondColor;
+            }
+
+            .ms_slot_link_footer_left,
+            .ms_slot_link_footer_right {
+                width: 100%;
+            }
+        }
+    }
+}
+
+// ========================================
 </style>
