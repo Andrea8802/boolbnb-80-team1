89bac20ab42473976d4bcaea3b8e595ca01c543d
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-03-18 19:56:34 +0100

    height

--- resources/js/components/Apartments/advancedSearch.vue
+++ resources/js/components/Apartments/advancedSearch.vue
@@ -2,24 +2,40 @@
     <div class="container-fluid">
         <!-- container del form -->
         <div class="container mt-5 text-center">
-            <h1 id="msTitlePage" class="ms_home_title my-3">Select search filters</h1>
+            <h1 id="msTitlePage"
+                class="ms_home_title my-3">Select search filters</h1>
             <div class="ms_ctn_form">
-                <form action="" method="post">
+                <form action=""
+                    method="post">
 
-                    <input type="text" v-model="apartmentSearch" placeholder="Enter your destination..."
+                    <input type="text"
+                        v-model="apartmentSearch"
+                        placeholder="Enter your destination..."
                         class="ms_search_bar form-control mb-3">
                     <div>
-                        <label for="" class="ms_label_ni">Rooms number </label>
-                        <input type="number" name="rooms_num" v-model="roomsNum" class="ms_number_bar form-control mb-3">
+                        <label for=""
+                            class="ms_label_ni">Rooms number </label>
+                        <input type="number"
+                            name="rooms_num"
+                            v-model="roomsNum"
+                            class="ms_number_bar form-control mb-3">
                     </div> <br>
 
                     <div>
-                        <label for="" class="ms_label_ni">Beds number </label>
-                        <input type="number" name="rooms_num" v-model="bedsNum" class="ms_number_bar form-control mb-3">
+                        <label for=""
+                            class="ms_label_ni">Beds number </label>
+                        <input type="number"
+                            name="rooms_num"
+                            v-model="bedsNum"
+                            class="ms_number_bar form-control mb-3">
                     </div> <br>
 
                     <div class="slidecontainer">
-                        <input type="range" min="1" max="50" class="slider" v-model="radius">
+                        <input type="range"
+                            min="1"
+                            max="50"
+                            class="slider"
+                            v-model="radius">
                     </div>
                     <div class="ms_radius">
                         Radius : {{ radius }}KM
@@ -29,12 +45,16 @@
                         <h4>Select services:</h4>
                         <div class="ms_ctn_check">
                             <div v-for="service in services">
-                                <input type="checkbox" :value="service.id" name=services v-model="modelServices">
+                                <input type="checkbox"
+                                    :value="service.id"
+                                    name=services
+                                    v-model="modelServices">
                                 <label for="services">{{ service.name }}</label>
                             </div>
                         </div>
                     </div>
-                    <button @click="getCoordinates" class="ms_btn_search">Search</button>
+                    <button @click="getCoordinates"
+                        class="ms_btn_search">Search</button>
                 </form>
                 <div class="container text-center">
                     <h4 class="ms_error_messagge">{{ error }}</h4>
@@ -45,11 +65,14 @@
         <!-- container delle card -->
         <div
             class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 align-items-stretch g-2 g-lg-3 mt-5 ms_ctn_card_home">
-            <div class="col ms_slot_card" v-for="apartment in apartmentsSponsored">
-                <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }" class="router">
+            <div class="col ms_slot_card"
+                v-for="apartment in apartmentsSponsored">
+                <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }"
+                    class="router">
                     <div class="card rounded ms_card_efct border-0">
                         <div class="ms_slot_card_img">
-                            <img :src="'/storage/' + apartment.imageApartment" :alt="apartment.title"
+                            <img :src="'/storage/' + apartment.imageApartment"
+                                :alt="apartment.title"
                                 class="rounded-5 fluid card-img-top">
                         </div>
                         <div class="ms_card_sponsored">Sponsored</div>
@@ -63,11 +86,14 @@
                 </router-link>
             </div>
 
-            <div class="col ms_slot_card" v-for="apartment in apartments">
-                <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }" class="router">
+            <div class="col ms_slot_card"
+                v-for="apartment in apartments">
+                <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }"
+                    class="router">
                     <div class="card rounded ms_card_efct border-0">
                         <div class="ms_slot_card_img">
-                            <img :src="'/storage/' + apartment.imageApartment" :alt="apartment.title"
+                            <img :src="'/storage/' + apartment.imageApartment"
+                                :alt="apartment.title"
                                 class="rounded-5 fluid card-img-top">
                         </div>
                         <div class="card-body h-35 text-start ps-2">
@@ -385,7 +411,7 @@ export default {
     .ms_slot_card_img {
 
         img {
-            height: 230px;
+            height: 260px;
         }
     }
 }
@@ -437,4 +463,5 @@ export default {
     }
 }
 
-// =====================================</style>
+// =====================================
+</style>
