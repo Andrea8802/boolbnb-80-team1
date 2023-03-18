6a2eeb554134f86e813cca2c297fcb08999c8ac6
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-03-18 17:14:03 +0100

    Personalizzati i breakpoint di bootstrap per il responsive delle card;

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="container text-center">
         <!-- titolo della pagina -->
-        <h1 id="msTitlePage"
-            class="ms_home_title my-3 text-black">HI! Where do you want to go today?</h1>
+        <h1 id="msTitlePage" class="ms_home_title my-3 text-black">HI! Where do you want to go today?</h1>
 
         <!-- sottotitolo della pagina -->
         <h4 class="ms_home_subtitle mb-3 text-capitalize">your destination awaits: discover new and amazing experiences</h4>
@@ -11,21 +10,15 @@
     <!-- container della barra di ricerca e pulsante ricerca avanzata -->
     <div class="container text-center">
         <div class="ms_ctn_search input-group">
-            <input type="text"
-                name="apartmentSearch"
-                v-model="apartmentSearch"
-                @keydown.enter="getCoordinates"
-                @keyup.delete="checkSearchBar"
-                placeholder="Enter your destination..."
+            <input type="text" name="apartmentSearch" v-model="apartmentSearch" @keydown.enter="getCoordinates"
+                @keyup.delete="checkSearchBar" placeholder="Enter your destination..."
                 class="ms_search_bar form-control border-end-0">
-            <button @click="getCoordinates"
-                class="input-group-text ms_btn_search border-start-0">
+            <button @click="getCoordinates" class="input-group-text ms_btn_search border-start-0">
                 <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
             </button>
         </div>
         <div class="my-3">
-            <button @click="deleteText"
-                class="ms_btn_delete me-3 principal">Reset</button>
+            <button @click="deleteText" class="ms_btn_delete me-3 principal">Reset</button>
             <router-link :to="{ name: 'advancedSearch' }">
                 <button class="ms_btn_advanced principal">
                     Advanced Search
@@ -38,15 +31,12 @@
     <!-- container principale di tutte le card degli appartamenti -->
     <div class="container-fluid mt-5 ms_ctn_card_home">
         <div>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5 align-items-stretch g-2 g-lg-3"
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 align-items-stretch g-2 g-lg-3"
                 v-if="!onSearch">
-                <div class="col ms_slot_card"
-                    v-for="apartment in apartmentsSponsored">
-                    <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }"
-                        class="router">
+                <div class="col ms_slot_card" v-for="apartment in apartmentsSponsored">
+                    <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }" class="router">
                         <div class="card h-100 rounded ms_card_efct">
-                            <img :src="'/storage/' + apartment.imageApartment"
-                                :alt="apartment.title"
+                            <img :src="'/storage/' + apartment.imageApartment" :alt="apartment.title"
                                 class="rounded fluid card-img-top h-50">
                             <div class="ms_card_sponsored">Sponsored</div>
                             <div class="card-body h-35">
@@ -58,13 +48,10 @@
                     </router-link>
                 </div>
 
-                <div class="col ms_slot_card"
-                    v-for="apartment in apartments">
-                    <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }"
-                        class="router">
+                <div class="col ms_slot_card" v-for="apartment in apartments">
+                    <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }" class="router">
                         <div class="card h-100 rounded ms_card_efct">
-                            <img :src="'/storage/' + apartment.imageApartment"
-                                :alt="apartment.title"
+                            <img :src="'/storage/' + apartment.imageApartment" :alt="apartment.title"
                                 class="rounded fluid card-img-top h-50">
                             <div class="card-body h-35">
                                 <h5 class="card-title text-center ms_aps_text">{{ apartment.title }}</h5>
@@ -76,15 +63,12 @@
                 </div>
             </div>
 
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5 align-items-stretch g-2 g-lg-3"
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-6 align-items-stretch g-2 g-lg-3"
                 v-else>
-                <div class="col ms_slot_card"
-                    v-for="apartment in apartmentsGeoSponsored">
-                    <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }"
-                        class="router">
+                <div class="col ms_slot_card" v-for="apartment in apartmentsGeoSponsored">
+                    <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }" class="router">
                         <div class="card h-100 rounded ms_card_efct">
-                            <img :src="'/storage/' + apartment.imageApartment"
-                                :alt="apartment.title"
+                            <img :src="'/storage/' + apartment.imageApartment" :alt="apartment.title"
                                 class="rounded fluid card-img-top h-50">
                             <div class="ms_card_sponsored">Sponsored</div>
                             <div class="card-body h-35">
@@ -96,13 +80,10 @@
                     </router-link>
                 </div>
 
-                <div class="col ms_slot_card"
-                    v-for="apartment in apartmentsGeo">
-                    <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }"
-                        class="router">
+                <div class="col ms_slot_card" v-for="apartment in apartmentsGeo">
+                    <router-link :to="{ name: 'detailApartment', params: { id: apartment.id } }" class="router">
                         <div class="card h-100 rounded ms_card_efct">
-                            <img :src="'/storage/' + apartment.imageApartment"
-                                :alt="apartment.title"
+                            <img :src="'/storage/' + apartment.imageApartment" :alt="apartment.title"
                                 class="rounded fluid card-img-top h-50">
                             <div class="card-body h-35">
                                 <h5 class="card-title text-center ms_aps_text">{{ apartment.title }}</h5>
