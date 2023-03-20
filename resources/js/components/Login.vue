d751ed01233d47b6e4616c97eb9ab2877e745f5c
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-03-20 15:46:47 +0100

    fix responsive login.vue;

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,53 +1,39 @@
 <template>
     <div class="container top-mrg">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card border-danger">
+            <div class="col-12 col-md-9 col-lg-6">
+                <div class="card border-0 shadow mx-auto ms_card_login">
                     <div class="card-header text-center display-6 bg-white border-white my-3">Login to your account</div>
 
                     <div class="card-body">
-                        <form action=""
-                            novalidate
-                            @submit.prevent="loginUser">
+                        <form action="" novalidate @submit.prevent="loginUser">
                             <p class="center">
                             <div class="red">{{ error }}</div>
 
                             </p>
 
                             <div class="row mb-3">
-                                <label for="email"
-                                    class="col-md-4 col-form-label text-md-end">Email</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
 
                                 <div class="col-md-6">
-                                    <input id="email"
-                                        type="email"
-                                        class="form-control"
-                                        name="email"
-                                        v-model="form.email"
-                                        autocomplete="email"
-                                        autofocus>
+                                    <input id="email" type="email" class="form-control" name="email" v-model="form.email"
+                                        autocomplete="email" autofocus>
                                 </div>
                             </div>
 
                             <div class="row mb-3">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-end">Password</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
 
                                 <div class="col-md-6">
-                                    <input id="password"
-                                        type="password"
-                                        class="form-control"
-                                        name="password"
-                                        v-model="form.password"
-                                        autocomplete="current-password">
+                                    <input id="password" type="password" class="form-control" name="password"
+                                        v-model="form.password" autocomplete="current-password">
 
                                 </div>
                             </div>
 
                             <div class="log-cont">
                                 <!-- <div class="col-md-12 my-4"> -->
-                                <button type="submit"
-                                    class="btn btn-outline-danger btn-lg">
+                                <button type="submit" class="btn btn-outline-danger btn-lg">
                                     Login
                                 </button>
                                 <!-- </div> -->
@@ -92,6 +78,10 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.ms_card_login {
+    margin-top: 100px;
+}
+
 .top-mrg {
     padding-top: 50px;
 }
